a70357ba14251354cf6799c3ee2d55050b9b1134
obatarhe <degraftekyiref@gmail.com>
2022-06-08 07:50:34 -0700

    task 4, hello puls

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
+// new function
 int main(void)
 
 {
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
-
+	// the puts function only accept string
 	puts(myStr);
 
 	return (0);
