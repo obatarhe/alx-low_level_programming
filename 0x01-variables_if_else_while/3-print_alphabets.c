68ccdc161fa516111279ed12c480e7ea0c1a4489
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-13 23:40:36 +0000

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char lowercase;
+  char uppercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+
+  for (uppercase = 'a'; uppercase <= 'z'; uppercase++)
+  putchar(uppercase);
+
+	putchar('\n');
+
+	return (0);
+}
