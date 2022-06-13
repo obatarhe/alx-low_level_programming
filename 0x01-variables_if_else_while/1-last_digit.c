f565bf4b86da69f46b94ce8898f699c381c1797d
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-13 22:57:32 +0000

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,16 @@
+/**
+*file name: 1-last_digit.c
+*Author: Ekyirefi O. Degraft
+*/
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
 *This program will assign a random number to the variable n each time it is executed.
-*Complete the source code in order to print the last digit of the number stored 
-*in the variable n. 
+*
+*Return: Always 0.
 */
 
 int main(void)
@@ -17,7 +21,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-    	if ((n % 10) > 5)
+	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
