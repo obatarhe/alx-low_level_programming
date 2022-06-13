6fad99e13a8720141fc40e564becb6311ff6836c
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-13 02:13:19 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
 /**
- * 8. UNIX is basically a simple operating system, 
- * but you have to be a genius to understand the simplicity
+ *8. UNIX is basically a simple operating system, 
+ *but you have to be a genius to understand the simplicity
  *
- * Return: Always 1.
+ *Return: Always 1.
  */
 
 #include <unistd.h>
 
 /**
- * Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
+ *Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ *followed by a new line, to the standard error.
  *
- * Return: Always 1.
+ *Return: Always 1.
  */
  
 int main(void)
