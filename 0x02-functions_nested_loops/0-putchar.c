d535800496b042a551d7037ae053b7948de0ca72
obatarhe <degraftekyiref@gmail.com>
2022-06-14 15:25:23 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * * main - prints all possible different combinations of two digits.
+ * * Numbers must be separated by ,, followed by a space
+ * * The two digits must be different
+ * *
+ * * Return: Always 0.
+ * */
+int main() {
+
+	    printf("_putchar\n");
+
+	        return 0;
+}
