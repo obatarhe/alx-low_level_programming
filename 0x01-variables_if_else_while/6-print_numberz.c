9959dcf1ed3cedca0122557a99a54648af79ea9d
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-14 01:36:49 +0000

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,13 @@
 
 int main(void)
 {
-  int num;
 
-  for (num = 0; num < 10; num++)
-    putchar((num % 10) + '0');
+	int num;
 
-  putchar('\n');
+		for (num = 0; num < 10; num++)
+			putchar((num % 10) + '0');
 
-  return (0);
+		putchar('\n');
+
+		return (0);
 }
