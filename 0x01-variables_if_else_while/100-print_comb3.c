6f93555028fbb2032f468b739c5395ddbc970edf
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-14 21:10:48 +0000

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - a program that prints all possible different combinations of two digits.
+* main - prints all possible different combinations of two digits.
 * Numbers must be separated by ,, followed by a space
 * The two digits must be different
 *
@@ -10,24 +10,23 @@
 
 int main(void)
 {
-  int digit1, digit2;
+	int digit1, digit2;
 
-  for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-    for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-    {
-      putchar((digit1 % 10) + '0');
-      putchar((digit2 % 10) + '0');
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-      if (digit1 == 8 && digit2 == 9)
-        continue;
-      
-      putchar(',');
-      putchar(' ');
-    }
-  }
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-  putchar('\n');
+	putchar('\n');
 
-  return (0);
 }
