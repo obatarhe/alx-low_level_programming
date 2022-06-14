d161e612c05bcff69686dc5de0393e00560bb516
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-14 01:43:05 +0000

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ *  followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+		char lowercase;
+
+		for (lowercase = 'z'; lowercase >= 'a'; lowercase--)
+		{
+				putchar(lowercase);
+		}
+
+		putchar('\n');
+
+		return (0);
+}
