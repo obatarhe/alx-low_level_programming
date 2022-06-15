0169fdb5723b06d393db747cfd08991b130915dc
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-15 00:28:01 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 
+void print_alphabet(void)
+{
+  char c = 'a';
+  while (c <= 'z')
+  {
+    _putchar(c);
+    c++;
+  }
+  _putchar('\n');
+}
