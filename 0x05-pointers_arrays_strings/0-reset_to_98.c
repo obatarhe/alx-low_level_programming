40dc53bf8379f0c7f05e02007b78ba78ee022815
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-15 02:24:57 +0000

    edit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+void reset_to_98(int *n)
+{
+	int *num;
+
+	int x = 98;
+
+	num = &x;
+	*n = *num;
+}
