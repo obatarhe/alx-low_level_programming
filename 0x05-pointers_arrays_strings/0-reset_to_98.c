0b2f0e37bb0d46fb4cee38e026a3487dd1fde69e
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-15 19:22:05 +0000

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter
+ * reset_to_98 - function that takes a pointer to an int
  * @n: pointer
  * Return: void
  */
