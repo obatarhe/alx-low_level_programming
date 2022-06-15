12ea4d4462e599b183c9cfdf6cdbc675f673192d
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-15 13:44:23 +0000

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * Write a function that checks for lowercase
- * Prototype: int _islower
- *
- * Return: 1 if lowercase
- */
+* This is the preferred style for multi-line
+* comments in C source code.
+* Please use it consistently.
+*
+* Description:  A column of asterisks on the left side,
+* with beginning and ending almost-blank lines.
+*/
 
 int _islower(int c)
 {
