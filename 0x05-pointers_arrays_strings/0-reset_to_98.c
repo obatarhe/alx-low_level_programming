c1dd43cda2fd036dca2ccbbf76e5d93dfcf6b5d5
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-15 19:17:45 +0000

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,26 +1,28 @@
 #include "main.h"
+
 /**
  * _strcat - concatenates two strings,
  * @dest: destination.
  * @src: source.
  * Return: the pointer to dest.
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	int con1 = 0, con2 = 0;
 
-	while (*(dest + count) != '\0')
+	while (*(dest + con1) != '\0')
 	{
-		count++;
+		con1++;
 	}
 
-	while (count2 >= 0)
+	while (con2 >= 0)
 	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
+		*(dest + con1) = *(src + con2);
+		if (*(src + con2) == '\0')
 			break;
-		count++;
-		count2++;
+		con1++;
+		con2++;
 	}
 	return (dest);
 }
