a221b5780a80eb20692835e20d54e1c60473e754
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-17 22:04:11 +0000

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,40 +1,36 @@
-/*
- * File: 101-print_comb4.c
- * Auth: Brennan D Baraban
- */
-
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - function that prints an integer.
- *        in ascending order
- *
- * Return: Always 0.
+ * print_number - function that prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-int main(void)
+
+void print_number(int n)
 {
-	int digit1, digit2, digit3;
+	unsigned int m, d, count;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	if (n < 0)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
-
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
 	}
 
-	putchar('\n');
+	d = m;
+	count = 1;
 
-	return (0);
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
