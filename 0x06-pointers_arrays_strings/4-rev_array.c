d55211c7add391c9711d850fe8ecc3227fb4277d
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-18 01:56:38 +0000

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * reverse_array - function that reverses the content 
- * of an array of integers.
+ * reverse_array - reverses the content of an array
+ * of integers.
  * @a: array.
  * @n: number of elements of the array.
  * Return: no return.
