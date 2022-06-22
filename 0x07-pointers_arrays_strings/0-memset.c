6e67cafe8c4e7e6ddbd03165e80cc1f9864360cd
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-22 04:07:12 +0000

    task 0, 0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,12 +7,14 @@
  * @n: an integer
  * Return: a string
  */
+
 char *_memset(char *s, char b, unsigned int n);
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
+	{
 		s[a] = b;
-
+	}
 	return (s);
 }
