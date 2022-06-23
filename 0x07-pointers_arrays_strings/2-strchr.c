1aad8d2248df99e59e4b77fb510fda24a6bb9412
obatahre <degraftekyirefi@gmail.com>
2022-06-23 03:38:20 +0000

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr - string character
+* @s: string given
+* @c: another char
+* Return: a string
+*/
+cahr *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+		a++;
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+			s += b;
+		return s;
+	}
+
+	return '\0'
+
+}
