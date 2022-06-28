a2eb3cb7a5d85053aefe80a8d9d2234dcb5c95a3
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-28 03:09:40 +0000

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,11 +4,11 @@
  * main - this a function to print its name
  * and file path should not be changed
  * @argc: argc parameter
- * @argc: an array of a command listed
+ * @argv: an array of a command listed
  * Return: 0 for success
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
