81b4c1765ddb727cf41a54aa8b38579257985f49
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-28 03:46:27 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,14 @@
 #include "stdio.h"
 
-/*
+/**
  * main - this a function to print its name
  * and file path should not be changed
  * @argc: argc parameter
- * @argc: an array of a command listed
+ * @argv: an array of a command listed
  * Return: 0 for success
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 
 {
 	int result = 0, num, i, j, k;
