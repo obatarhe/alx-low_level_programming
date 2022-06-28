b58ab4af3b24a4069a1e801234239fd5561acc6e
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-28 03:16:17 +0000

    betty checked

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 /**
  * main - this a function to print its name
  * @argc: argc parameter
- * @argv: an array of a command listed
+ * @argv: an array of a command list
  * Return: 0 for success
  */
 
