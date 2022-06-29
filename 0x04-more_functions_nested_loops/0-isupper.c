d82b224d9ca8d01bba00b28bb5625a6f6ae7c4d2
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-29 03:01:32 +0000

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,10 +3,15 @@
 /**
 * _isupper - function that checks for uppercase character
 * @c: An input character
+*
 * Return: 1 if c is uppercase or 0 
 */
 
 int _isupper(int c)
 {
-	return(>= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
 }
