264ef66a00f35c2849b9eea51a6132b02991910d
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-06-30 16:55:21 +0000

    fixing betty

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
 
 void print_numbers(void)
 {
-    int x;
+	int x;
 
-    for (x = 0; x < 10; x++)
-        _putchar(x + '0');
-    _putchar('\n');
+	for (x = 0; x < 10; x++)
+		_putchar(x + '0');
+	_putchar('\n');
 
 }
