d216c39a6325c46a4546c6fa578c337bef91e7a3
obatahre <degraftekyirefi@gmail.com>
2022-06-30 13:52:09 +0000

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,21 @@
-#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
+ * int_isupper - Checks for uppercase character
+ * @c: the character to be checked
+ * return 1 1 if uppercase else 0
  */
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
 
-	else
+int _isupper(int c);
+{
+	int c;
+
+	if (c >= 'A' && c <+'Z') {
+		return (1);
+	}
+	else {
 		return (0);
+	}
 }
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/* this prototype has to be declared in a header file */
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
