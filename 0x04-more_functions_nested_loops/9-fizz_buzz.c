20dd588357b3ff737ad310aaf87c34d48420859c
obatahre <degraftekyirefi@gmail.com>
2022-07-01 14:41:51 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,22 @@
 
 void fizz_buzz(void)
 {
-	int num;
+	int x;
 
-	for (num = 1; num <= 100; num++)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
+		if ((x % 3 == 0) && (x % 5 == 0))
+			printf("FizzBuzz ");
 
-		else if ((num % 3) == 0)
-			printf("Fizz");
+		else if (x % 3 == 0)
+			printf("Fizz ");
 
-		else if ((num % 5) == 0)
-			printf("Buzz");
-
+		else if (x % 5 == 0)
+			printf("Buzz ");
+			
 		else
-			printf("%d ", num);
+			printf("%d ", x);
 	}
+	printf("\n")
+	return (0);
 }
