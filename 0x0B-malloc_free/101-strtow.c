eb265e72a560d53a65b2d40831eb2cc274b44955
obatahre <degraftekyirefi@gmail.com>
2022-07-08 01:52:35 +0000

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,7 +6,7 @@ int count_words(char *str);
 char **strtow(char *str);
 
 /**
- * word_len - Locates the index marking the end of the first word contained within a string.
+ * word_len - Locates the index marking the end of the first word
  * @str: The string to be searched.
  *
  * Return: The index marking the end of the initial word pointed to by str.
