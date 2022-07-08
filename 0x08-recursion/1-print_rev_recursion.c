ec575ae3ec533f2e326d85ea8ebac5dd09ff9b66
obatahre <degraftekyirefi@gmail.com>
2022-07-08 03:42:38 +0000

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
 /**
- * Print a string using recursion
- * @s: string parameter
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *         Otherwise - a pointer to the duplicated string.
  */
 
 void _print_rev_recursion(char *s)
