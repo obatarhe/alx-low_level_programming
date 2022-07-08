57a9ff858b09906bc408c230d60be99af1c5be70
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-07-08 03:49:55 +0000

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,10 @@
  * Return: length of string
  */
 
-int _strlen_recursion (char *s)
+int _strlen_recursion(char *s)
 {
     if (*s == '\0')
-        return (0);
-    s++;
-    return (1 + _strlen_recursion(s));
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
