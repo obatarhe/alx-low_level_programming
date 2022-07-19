37389da4361355e3235bb2f05908572b46350ac9
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-07-19 06:14:31 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,11 +8,11 @@
 
 void print_char(va_list arg)
 {
-    char letter;
+	char letter;
 
-    letter = va_arg(arg, int);
+	letter = va_arg(arg, int);
 
-    printf("%c", letter);
+	printf("%c", letter);
 }
 
 /**
@@ -23,11 +23,11 @@ void print_char(va_list arg)
 
 void print_int(va_list arg)
 {
-    int num;
+	int num;
 
-    num = va_arg(arg, int);
+	num = va_arg(arg, int);
 
-    printf("%d", num);
+	printf("%d", num);
 }
 
 /**
@@ -35,13 +35,14 @@ void print_int(va_list arg)
 * @arg: A list of arguments pointing to
 * the float to be printed.
 */
+
 void print_float(va_list arg)
 {
-    float num;
+	float num;
 
-    num = va_arg(arg, double);
+	num = va_arg(arg, double);
 
-    printf("%f", num);
+	printf("%f", num);
 }
 
 /**
@@ -52,17 +53,17 @@ void print_float(va_list arg)
 
 void print_string(va_list arg)
 {
-    char *str;
+	char *str;
 
-    str = va_arg(arg, char *);
+	str = va_arg(arg, char *);
 
-    if (str == NULL)
-    {
-        printf("(nil)");
-        return;
-    }
+	if (str == NULL)
+	{
+	printf("(nil)");
+		return;
+	}
 
-    printf("%s", str);
+	printf("%s", str);
 }
 
 /**
@@ -74,39 +75,39 @@ void print_string(va_list arg)
 
 void print_all(const char * const format, ...)
 {
-    va_list args;
+	va_list args;
 
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    char *separator = "";
+	char *separator = "";
 
-    printer_t funcs[] = {
-    {"c", print_char},
-    {"i", print_int},
-    {"f", print_float},
-    {"s", print_string}
-    };
+	printer_t funcs[] = {
+	{"c", print_char},
+	{"i", print_int},
+	{"f", print_float},
+	{"s", print_string}
+	};
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && (*(format + i)))
-    {
-        j = 0;
-        while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-            j++;
+	while (format && (*(format + i)))
+	{
+	j = 0;
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
 
-        if (j < 4)
-        {
-            printf("%s", separator);
-            funcs[j].print(args);
-            separator = ", ";
-        }
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
 
-        i++;
+		i++;
 
-    }
+	}
 
-    printf("\n");
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 }
