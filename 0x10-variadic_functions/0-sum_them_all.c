160e5fd7b4d8ba649ee96b290e6b8269d4ba81da
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-07-19 05:56:03 +0000

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,19 +11,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list li;
-	int sum = 0;
-	unsigned int i;
+	va_list nums;
+	unsigned int index, sum = 0;
 
-	va_start(li, n);
+	va_start(nums, n);
 
-	if (n != 0)
-	{
-		for (i = 0; i < n; i++)
-		sum += va_arg(li, int);
-	}
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
 
-	va_end(li);
+	va_end(nums);
 
 	return (sum);
 }
