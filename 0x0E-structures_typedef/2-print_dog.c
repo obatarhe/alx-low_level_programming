a407e49a06adf7cd3ac3ad7fc30732c23abaac1f
Ekyirefi O. Degraft <104810033+obatarhe@users.noreply.github.com>
2022-07-19 04:11:22 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,24 +6,23 @@
 *
 */
 
-
 void print_dog(struct dog *d)
 {
     if (d == NULL )
-        return; 
+		return;
 
-    if ((*d).name == NULL)
-        printf("nil\n");
-    else
-        printf("Name: %s\n", d->name);
+	if ((*d).name == NULL)
+		printf("nil\n");
+	else
+		printf("Name: %s\n", d->name);
+	
+	if ((*d).age < 0)
+		printf("nil\n");
+	else
+		printf("Age: %f\n", d->age);
 
-    if ((*d).age < 0)
-        printf("nil\n");
-    else
-        printf("Age: %f\n", d->age);
-
-    if ((*d).owner == NULL)
-        printf("nil\n");
-    else
-        printf("Owner: %s\n", d->owner);
+	if ((*d).owner == NULL)
+		printf("nil\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
